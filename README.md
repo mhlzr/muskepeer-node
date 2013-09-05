04e3c84cc0df068d08c93fa22f2492c21966ca11
casatt <matthieuholzer@gmx.de>
2013-09-05 14:35:13 +0200

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+musketeer-node
+================
+
+Auto-Connects via WebSocket to a musketeer-server, connects to all the other musketeer-nodes and handles WebRTC handshaking between the musketeer-clients.
+
---
+++ node.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Matthieu Holzer
+ * @version 0.1
+ */
+
+var io = require('socket.io').listen(80);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello : 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
