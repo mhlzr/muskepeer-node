a411d10544a9c454bfca5409b5175a8e3bdf1ce2
casatt <matthieuholzer@gmx.de>
2013-11-29 21:53:56 +0100

    Test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/bin/env node
 
 
-var  WebSocketServer = require('ws').Server;
+/*var  WebSocketServer = require('ws').Server;
 
 var wss = new WebSocketServer({
         port: process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8080;}
@@ -18,4 +18,20 @@ wss.on('connection', function (socket) {
 });
 
 
-console.log('Running');
+console.log('Running');*/
+
+
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  res.write('List:');
+  for(var key in process.env){
+	res.write(process.env[key]);
+  }
+  
+  res.end();
+  
+}).listen(8080);
