ecba82c323febd958e58dadf43cf9d004e949d5c
casatt <matthieuholzer@gmx.de>
2013-11-29 23:06:11 +0100

    ddd

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer(
     {
         host: process.env.OPENSHIFT_NODEJS_IP,
-        port: 8443
+        port: 8080
     }
 );
 
