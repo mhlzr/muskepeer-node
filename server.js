74123e44606678e229dce3ea801482ff2b6128bd
casatt <matthieuholzer@gmx.de>
2013-11-29 22:21:33 +0100

    f

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write(process.env.OPENSHIFT_NODEJS_PORT);
+  res.write(process.env.OPENSHIFT_NODEJS_IP);
   res.end('Test\n');
 }).listen(process.env.OPENSHIFT_NODEJS_PORT  || 8080, process.env.OPENSHIFT_NODEJS_IP);
 
