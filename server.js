4cee3c4f68e9ea5ea03437020af125d9d53b31ad
casatt <matthieuholzer@gmx.de>
2013-11-29 22:26:47 +0100

    F

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 #!/bin/env node
 //  OpenShift sample Node application
 
+var  WebSocketServer = require('ws').Server;
+
+var wss = new WebSocketServer({
+		host:process.env.OPENSHIFT_NODEJS_IP,
+        port: process.env.OPENSHIFT_NODEJS_PORT  || 8080;}
+);
+
+wss.on('connection', function (socket) {
+
+    socket.send('Hello');
+		
+    socket.on('message', function (data) {
+        console.log(data);
+    });
+
+});
+
+/*
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -8,4 +26,5 @@ http.createServer(function (req, res) {
   res.write(process.env.OPENSHIFT_NODEJS_IP);
   res.end('Test\n');
 }).listen(process.env.OPENSHIFT_NODEJS_PORT  || 8080, process.env.OPENSHIFT_NODEJS_IP);
+/*
 
